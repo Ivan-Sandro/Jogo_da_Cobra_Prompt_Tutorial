ddcc821970da1b061feac3fdf852aa03687a6019
Ivan Sandro <81819218+Ivan-Sandro@users.noreply.github.com>
2021-06-28 18:46:31 -0300

    Add files via upload

--- Display.h
+++ Display.h
@@ -9,7 +9,7 @@
 class _PLANO{
     private:
         unsigned char Mapa[20][20] =   {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+                                        2, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
                                         2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
                                         2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
                                         2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
@@ -29,14 +29,16 @@ class _PLANO{
                                         2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
                                         2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3};
 
-        unsigned char Estruturas[4][2]   =  {219, 'B',   // Esquerda -> RepresentaÃ§Ã£o mapa
-                                             220, 'B',   // Meio     -> RepresentaÃ§Ã£o em ASCII
+        unsigned char Estruturas[4][2]   =  {219, 'B',   // Esquerda -> Representação mapa
+                                             220, 'B',   // Meio     -> Representação em ASCII
                                              223, 'B',   // Direita  -> Tipo de Estrutura
-                                             233, 'F'};  // 'B' -> Barreira || 'F' -> Fruta
+                                             208, 'F'};  // 'B' -> Barreira  || 'F' -> Fruta
 
     public:
         void _Desenhar_Plano(void);
+        void _Criar_Fruta(std::vector <unsigned char> &Posisoes_Invalidas_X, std::vector <unsigned char> &Posisoes_Invalidas_Y);
         unsigned char _Get_Estrutura_Mapa(unsigned char Mapa_X, unsigned char Mapa_Y, unsigned char Tipo_Estrutura);
+        void _Mudar_Valor_Mapa(unsigned char Mapa_X, unsigned char Mapa_Y, unsigned char Valor);
 
 
 };
