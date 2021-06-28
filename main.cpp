63a0ed0aa179af93a06d9f3635bf7b14eb947ba5
Ivan Sandro <81819218+Ivan-Sandro@users.noreply.github.com>
2021-06-28 18:46:36 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 int main()
 {
+    srand(time(NULL));
+
     _Desenhar_Menu();
     _getch();
 
     system("cls");
 
-    srand(time(NULL));
     _PLANO Janela;
     _PLAYER Jogador;
 
@@ -22,15 +23,17 @@ int main()
     Janela._Criar_Fruta(Jogador._Get_Posisoes_Cobra_X(), Jogador._Get_Posisoes_Cobra_Y());
     Janela._Desenhar_Plano();
 
-    while(Jogador._Get_Vida() > 0)
+    do
     {
         Jogador._Puxar_Corpo();
         Jogador._Direcionar_Player();
         Jogador._Mover_Player();
-        Jogador._Desenhar_Player();
         Jogador._Verificar_Impacto(Janela);
+        Jogador._Desenhar_Player();
+
         Sleep(100);
     }
+    while(Jogador._Get_Vida() > 0);
     _getch();
     return 0;
 }
