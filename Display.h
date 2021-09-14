5c8553cfd0708dbec8e43df94fdad3cb000b4352
Ivan Sandro <81819218+Ivan-Sandro@users.noreply.github.com>
2021-09-14 10:49:25 -0300

    Add files via upload

--- Display.h
+++ Display.h
@@ -6,40 +6,12 @@
 #include <vector>
 #include <ctime>
 
-class _PLANO{
-    private:
-        unsigned char Mapa[20][20] =   {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-                                        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3};
 
-        unsigned char Estruturas[4][2]   =  {219, 'B',  
-                                             220, 'B',   // Esquerda -> Representação em ASCII
-                                             223, 'B',   // Direita  -> Tipo de Estrutura
-                                             208, 'F'};  // 'B' -> Barreira  || 'F' -> Fruta
+void _Desenhar_Plano(void);
+void _Criar_Fruta(std::vector <unsigned char> &Posisoes_Invalidas_X, std::vector <unsigned char> &Posisoes_Invalidas_Y);
+unsigned char _Get_Estrutura_Mapa(unsigned char Mapa_X, unsigned char Mapa_Y, unsigned char Tipo_Estrutura);
+void _Mudar_Valor_Mapa(unsigned char Mapa_X, unsigned char Mapa_Y, unsigned char Valor);
 
-    public:
-        void _Desenhar_Plano(void);
-        void _Criar_Fruta(std::vector <unsigned char> &Posisoes_Invalidas_X, std::vector <unsigned char> &Posisoes_Invalidas_Y);
-        unsigned char _Get_Estrutura_Mapa(unsigned char Mapa_X, unsigned char Mapa_Y, unsigned char Tipo_Estrutura);
-        void _Mudar_Valor_Mapa(unsigned char Mapa_X, unsigned char Mapa_Y, unsigned char Valor);
-};
 
 void _gotoxy (short int X, short int Y);
 
